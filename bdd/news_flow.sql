de678d75153499a0dbfbb06fecf7c86a59bc577f
sauzanne <sauzanne@sauzanne-XPS.ffbb.fr>
2020-02-04 19:09:09 +0100

    + modification du news flow

--- bdd/news_flow.sql
+++ bdd/news_flow.sql
@@ -1,17 +1,18 @@
 CREATE TABLE `news_flow` (
-  `id` int unsigned NOT NULL,
-  `name` nvarchar(255) NOT NULL,
-  `url` nvarchar(300) NOT NULL,
+  `id` int unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(300) NOT NULL,
   `keyword` text NOT NULL,
-  `notification` tinyint(1) unsigned NOT NULL,
+  `notification` tinyint unsigned NOT NULL,
   `account_id` int unsigned NOT NULL,
   `first_input` datetime NOT NULL,
-  `last_modified` datetime NULL
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8 ROW_FORMAT = DEFAULT;
+  `last_modified` datetime NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB ROW_FORMAT = DEFAULT;
 
 create index news_flow_ix_account_id on news_flow(account_id);
 
 alter table news_flow add CONSTRAINT news_flow_fk_account_id foreign key news_flow_ix_account_id(account_id) references account(id);
 
-INSERT INTO `news_flow` (`id`, `name`, `url`, `keyword`, `notification`, `account_id`, `first_input`, `last_modified`) VALUES
-(18, 'boursier.com', 'http://www.boursier.com/(S(tluffdbozlfm3okioeqgux5u))/syndication/atom/news', 'plastivaloire, abc arbitrage, opr, velcan energy, grèce, albioma, aurea, bigben,faiveley, actia,\n bigben, platine, platinum, orange, business et decision, bnd, futuren, parrot, eo2, mpi, brésil,\n amérique latine, alstom, systran, amazon, vergnet, velcan, mr bricolage, sfr, tesla, yandex, derichebourg, porsche, gaumont, GIROD, microwave, sword, vergnet, qiwi, bouygues, peugeot, renault, ubi,volkswagen, maurel, neopost, maurel et prom, ubisoft, engie,\n cnim, engie, pizzorno, fnac, umanis, michelin, brexit, signaux girod,\n iliad, Business et Decision, Aufeminin.com, automobile, archos, it link, umanis, trump, itlink,\n samsung, pétrole, voltalia, coface, latecoere, graines voltz, solutions 30, sqli, ile et vilaine, innate pharma, ose immuno, ose immunotherapeutics, astellia, opa, zodiac, sanofi, lg display, median,\nsolocal, erytech, peugeot, psa, maurel,eramet, free, pétrole, environnement sa, prodways, ald, deutsche bank, micropole, aures, gemalto,technipfmc, cgg, carrefour, sqli, arcelormittal, société générale,1000mercis, telefonica, spotify, dnxcorp, navya, michelin', 1, 80, '2015-10-27 12:01:29', '2018-07-10 07:24:23');
+INSERT INTO `news_flow` (`name`, `url`, `keyword`, `notification`, `account_id`, `first_input`, `last_modified`) VALUES
+('boursier.com', 'http://www.boursier.com/(S(tluffdbozlfm3okioeqgux5u))/syndication/atom/news', 'plastivaloire, abc arbitrage, albioma, aurea, bigben, orange, eo2, velcan, sword, bouygues, renault, quadient, maurel et prom, engie, cnim, pizzorno, fnac, iliad, it link, umanis, itlink, samsung, pétrole, automobile, voltalia, graines voltz, sqli, ose immuno, ose immunotherapeutics, erytech,eramet, prodways, deutsche bank, aures, société générale, telefonica, spotify, canadian solar, chili,veolia, derichebourg, tesla, precia, infotel, sopra steria, actia, first solar, sanofi, atos, claranova, worldline, solocal, guillemot, envea, seche, innate, ipsen, lna santé, casino, mediawan, lvmh, EssilorLuxottica, ubisoft, solutions 30, rallye, moulinvest, altice, luxe, parrot, marimekko, banque, banques, worldline, seb, michelin, maisons du monde, netflix, uber, hermes, maison du monde', 1, 80, '2015-10-27 12:01:29', '2018-07-10 07:24:23');
